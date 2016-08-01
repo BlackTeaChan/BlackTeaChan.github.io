03cb1a6739af0e95cb4f9d72f060f06f0c6ccdd1
陈宏察 <435661630@qq.com>
2016-08-02 01:01:17 +0800

    斗地主0.1beta
    加入生成54张扑克牌的GetPoker类

---
+++ JavaGame/DouDiZhu/src/MyClass.java
@@ -0,0 +1,27 @@
+public class MyClass {
+	// ºÚÌÒ£¨Spade£©
+	// ºìÌÒ£¨Heart£©
+	// ·½¿é£¨Diamond£©
+	// Ã·»¨£¨Club£©
+	public static void main(String[] ages) {
+		GetPoker gp=new GetPoker();
+
+	}
+
+}
+
+class GetPoker {
+	String color[] = { "S", "H", "D", "C" };
+	char level[] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
+	String[] poker = new String[55];
+	int i = 1;{
+	for (int k = 0; k < 13; k++){
+		for (int j = 0; j < 4; j++) {
+			poker[i++] = (color[j] + level[k]);
+		}
+	}
+	poker[53]="DK";
+	poker[54]="Sk";
+	for(i=1;i<55;i++)
+	System.out.println("poker[" + i + "]:" + poker[i]);
+}}
