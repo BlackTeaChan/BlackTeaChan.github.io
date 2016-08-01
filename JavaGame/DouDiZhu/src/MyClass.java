f340e5b6040b653f2edcd7822c6423ef0d3915eb
陈宏察 <435661630@qq.com>
2016-08-02 01:21:54 +0800

    修改部分代码
    修改大小王String名称为“J1”，“J2”。

--- JavaGame/DouDiZhu/src/MyClass.java
+++ JavaGame/DouDiZhu/src/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass {
 	// ·½¿é£¨Diamond£©
 	// Ã·»¨£¨Club£©
 	public static void main(String[] ages) {
-		GetPoker gp=new GetPoker();
+		GetPoker gp = new GetPoker();
 
 	}
 
@@ -12,16 +12,19 @@ public class MyClass {
 
 class GetPoker {
 	String color[] = { "S", "H", "D", "C" };
-	char level[] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
+	char level[] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J',
+			'Q', 'K' };
 	String[] poker = new String[55];
-	int i = 1;{
-	for (int k = 0; k < 13; k++){
-		for (int j = 0; j < 4; j++) {
-			poker[i++] = (color[j] + level[k]);
+	int i = 1;
+	{
+		for (int k = 0; k < 13; k++) {
+			for (int j = 0; j < 4; j++) {
+				poker[i++] = (color[j] + level[k]);
+			}
 		}
+		poker[53] = "J1";
+		poker[54] = "J2";
+		// for(i=1;i<55;i++)
+		// System.out.println("poker[" + i + "]:" + poker[i]);
 	}
-	poker[53]="DK";
-	poker[54]="Sk";
-	for(i=1;i<55;i++)
-	System.out.println("poker[" + i + "]:" + poker[i]);
-}}
+}
