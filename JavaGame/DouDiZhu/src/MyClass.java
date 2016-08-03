df26cd918f2ccbc8bed0a72c007b3e6009f98afc
陈宏察 <435661630@qq.com>
2016-08-03 19:14:18 +0800

    斗地主0.0beta
    添加运行类；
    添加获取地主牌的方法；
    修改部分代码；

--- JavaGame/DouDiZhu/src/MyClass.java
+++ JavaGame/DouDiZhu/src/MyClass.java
@@ -4,14 +4,8 @@ public class MyClass {
 	// 方块（Diamond）
 	// 梅花（Club）
 	public static void main(String[] ages) {
-		GetPoker gp = new GetPoker();
-		gp.TestPoker();
-		gp.GetPlayer1();
-		gp.GetPlayer2();
-		gp.GetPlayer3();
-		gp.TestPlayer1();
-		gp.TestPlayer2();
-		gp.TestPlayer3();
+		System.out.println("斗地主 0.0 beta    By BlackTea Chan");
+		Run run=new Run();
 	}
 }
 class GetPoker {
@@ -19,53 +13,70 @@ class GetPoker {
 	private int random;
 	String color[] = { "S", "H", "D", "C" };
 	char level[] = { '3', '4', '5', '6', '7', '8', '9', '0', 'J','Q', 'K', 'A', '2' };
-	String[] poker = new String[55];
+	String[] poker1 = new String[55];
+	String[] poker2 = new String[55];
 	String[] player1=new String[21];
 	String[] player2=new String[21];
 	String[] player3=new String[21];
+	String[] dizhu=new String[3];
 	GetPoker(){
 		for (int k = 0; k < 13; k++)
-			for (int j = 0; j < 4; j++)
-				poker[i++] = (color[j] + level[k]);
-		poker[53] = "J1";
-		poker[54] = "J2";
+			for (int j = 0; j < 4; j++){
+				poker1[i] = (color[j] + level[k]);
+				poker2[i++] = (color[j] + level[k]);
+			}
+		poker1[53] = "J1";
+		poker1[54] = "J2";
+		poker2[53] = "J1";
+		poker2[54] = "J2";
+		for(i=0;i<3;i++){
+			random=(int)(Math.random()*54+1);
+			if(poker1[random]=="void"){
+				i--;
+				continue;
+			}
+			dizhu[i]=poker1[random];
+			poker1[random]="void";
+		}
 	}
 	void GetPlayer1(){
 		for(i=1;i<=17;i++){
 			random=(int)(Math.random()*54+1);//随机生成一个数random，用来给player1发牌；
-			if(poker[random]=="void"){//检测到扑克牌为void时，说明已经发过这张牌，换一张；
+			if(poker1[random]=="void"){//检测到扑克牌为void时，说明已经发过这张牌，换一张；
 				i--;
 				continue;
 			}
-			player1[i]=poker[random];
-			poker[random]="void";//已经发过的牌String值改为“void”；
+			player1[i]=poker1[random];
+			poker1[random]="void";//已经发过的牌String值改为“void”；
 		}
 	}
 	void GetPlayer2(){
 		for(i=1;i<=17;i++){
 			random=(int)(Math.random()*54+1);
-			if(poker[random]=="void"){
+			if(poker1[random]=="void"){
 				i--;
 				continue;
 			}
-			player2[i]=poker[random];
-			poker[random]="void";
+			player2[i]=poker1[random];
+			poker1[random]="void";
 		}
 	}
 	void GetPlayer3(){
 		for(i=1;i<=17;i++){
 			random=(int)(Math.random()*54+1);
-			if(poker[random]=="void"){
+			if(poker1[random]=="void"){
 				i--;
 				continue;
 			}
-			player3[i]=poker[random];
-			poker[random]="void";
+			player3[i]=poker1[random];
+			poker1[random]="void";
 		}
 	}
+	
+	
 	void TestPoker(){
 		for(i=1;i<55;i++){
-			System.out.println("poker[" + i + "]:" + poker[i]);
+			System.out.println("poker[" + i + "]:" + poker1[i]);
 //			try {
 //				Thread.sleep(100);
 //	        } catch (InterruptedException e) {
@@ -74,8 +85,11 @@ class GetPoker {
 		}
 	}
 	void TestPlayer1(){
-		for(i=1;i<=17;i++)
-			System.out.println("player1[" + i + "]:" + player1[i]);
+		System.out.println("玩家一扑克牌：");
+		for(i=1;i<=17;i++){
+			if(player1[i]=="void")continue;
+			System.out.printf(player1[i]+" ");
+		}
 	}
 	void TestPlayer2(){
 		for(i=1;i<=17;i++)
@@ -84,5 +98,16 @@ class GetPoker {
 	void TestPlayer3(){
 		for(i=1;i<=17;i++)
 			System.out.println("player3[" + i + "]:" + player3[i]);
+	}
+	void TestDiZhu(){
+		System.out.println("地主牌："+dizhu[0]+","+dizhu[1]+","+dizhu[2]);
+	}
+}
+class Run extends GetPoker{
+	Run(){
+		this.GetPlayer1();
+		this.GetPlayer2();
+		this.GetPlayer3();
+		this.TestPlayer1();
 	}
 }
